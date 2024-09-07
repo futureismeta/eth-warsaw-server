bcf98477359777ab6ddf59aa860a0f0fc62a4381
Luke Watson <luke.connor.watson@gmail.com>
2024-09-07 13:20:13 +0200

    Add initial model

--- packages/mml-document/src/App.tsx
+++ packages/mml-document/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Model} from "./components/model";
 
 function App() {
     return (
         <m-group z={30}>
-
+            <Model/>
         </m-group>
     );
 }
---
+++ packages/mml-document/src/components/model.tsx
@@ -0,0 +1,9 @@
+export const Model = () => {
+    return (
+        <m-group>
+            <m-model
+                src="/assets/buildings/model.glb"
+            ></m-model>
+        </m-group>
+    );
+};
