c029308b31d4cf616c90b196fdf404b4c135f433
Luke Watson <luke.connor.watson@gmail.com>
2024-09-27 18:22:59 +0800

    -

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 8080;
 
 export const characterDescription = {
-    // meshFileUrl: '/assets/models/bot.glb',
-    meshFileUrl: 'http://localhost:8080/assets/models/bot.glb',
+    meshFileUrl: '/assets/models/bot.glb',
+    // meshFileUrl: 'http://localhost:8080/assets/models/bot.glb',
 };
